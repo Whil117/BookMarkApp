1c0166709c2052dfca59d9331bc5a8ddd1d223d2
Ivan <miltonivan117@gmail.com>
2021-06-16 16:45:13 -0600

    card as default export

--- src/pages/components/Extension/components/Card.tsx
+++ src/pages/components/Extension/components/Card.tsx
@@ -1,21 +1,25 @@
-import React,{FC} from 'react'
-import Image from 'next/image'
-import { CardChild } from '../../../../styles/Extension/ExtensionStyle'
-
+import React, { FC } from "react";
+import Image from "next/image";
+import { CardChild } from "../../../../styles/Extension/ExtensionStyle";
 
 interface Data {
-    img:any
-    title:string
-    descript:string
+  img: any;
+  title: string;
+  descript: string;
 }
 
-export const Card:FC<Data> = ({img,title,descript}) => {
-    return (
-        <CardChild initial={{ opacity: 0 }}  animate={{ x: 0, opacity: 1 }} transition={{ ease: "easeOut", duration: 2 }} >
-            <Image src={img} alt ={title} />
-            <h4>{title}</h4>
-            <p>{descript}</p>
-            <a href="#">Add & Install Extension</a>
-        </CardChild>
-    )
-}
+const Card: FC<Data> = ({ img, title, descript }) => {
+  return (
+    <CardChild
+      initial={{ opacity: 0 }}
+      animate={{ x: 0, opacity: 1 }}
+      transition={{ ease: "easeOut", duration: 2 }}
+    >
+      <Image src={img} alt={title} />
+      <h4>{title}</h4>
+      <p>{descript}</p>
+      <a href="#">Add & Install Extension</a>
+    </CardChild>
+  );
+};
+export default Card;
